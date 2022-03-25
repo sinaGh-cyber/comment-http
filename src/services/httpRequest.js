75eca0abb1c157053a3dab8642398b6e49028952
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-03-25 17:02:01 +0430

    getAllComments fixed + addNewComment, deleteCommen, updateComment funcs added

--- src/services/httpRequest.js
+++ src/services/httpRequest.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8000/comments';
+axios.defaults.baseURL = 'http://localhost:8000/commentsDUMMY';
 
 const httpRequests = {
   getAllComments() {
-    console.log(axios.get());
+    return axios.get();
+  },
+
+  addNewComment(body) {
+    return axios.post('', body);
+  },
+
+  deleteComment(id) {
+    return axios.delete(`/${id}`);
+  },
+
+  updateComment(id, body) {
+    return axios.put(`/${id}`, body);
   },
 };
 
