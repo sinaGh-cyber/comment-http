28ff6d1ca754d7f795864bdea6f7bdddb3027b4b
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-09 23:42:44 +0430

    routs.js file created

---
+++ src/routes.js
@@ -0,0 +1,62 @@
+import AboutUsPage from './pages/about-us/about-us';
+import BlogPage from './pages/blogPage/blogPage';
+import HomePage from './pages/home/home';
+import NotFoundPage from './pages/not-found/not-found';
+import PostPage from './pages/post/postPage';
+import ProfilePage from './pages/profile/profile';
+
+
+const mainRoutes = [
+  {
+    isExact: false,
+    component: PostPage,
+    path: '/blog/:id',
+    title: 'Post',
+    hasLink: false,
+    id: 0,
+  },
+
+  {
+    isExact: false,
+    component: AboutUsPage,
+    path: '/About-us/',
+    title: 'About us',
+    hasLink: true,
+    id: 1,
+  },
+  {
+    isExact: false,
+    component: ProfilePage,
+    path: '/profile/',
+    title: 'Profile',
+    hasLink: true,
+    id: 2,
+  },  {
+    isExact: false,
+    component: BlogPage,
+    path: '/blog',
+    title: 'Blog',
+    hasLink: true,
+    id: 3,
+  },
+  {
+    isExact: true,
+    component: HomePage,
+    path: '/',
+    title: 'Home',
+    hasLink: true,
+    id: 4,
+  },
+
+
+  {
+    isExact: false,
+    component: NotFoundPage,
+    path: '*',
+    title: 'Not found',
+    hasLink: false,
+    id: 5,
+  },
+];
+
+export { mainRoutes };
