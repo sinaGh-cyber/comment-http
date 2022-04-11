083e4ee68c59929e729c8b1bb00624b75cf6dd4d
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-04-11 20:26:24 +0430

    HomePage created and styled

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,24 @@
-import Navigation from "../../components/navigation/navigation";
-const HomePage = () => {
-  return <Navigation />;
+import { Link } from 'react-router-dom';
+import CommentPreview from '../../components/commentPreview/commentPreview';
+import styles from './HomePage.module.scss';
+const HomePage = ({ comments }) => {
+  return (
+    <section className={styles.commentGroupSection}>
+      {comments.length === 0 ? (
+        <p>No comments, please add a comment.</p>
+      ) : comments === null ? (
+        'Loading...'
+      ) : (
+        comments.map((comment) => {
+          return (
+            <Link key={comment.id} to={`/comment/${comment.id}`}>
+              <CommentPreview commentInfo={comment} />
+            </Link>
+          );
+        })
+      )}
+    </section>
+  );
 };
 
 export default HomePage;
